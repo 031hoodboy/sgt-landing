d174b706da25b550ec907010499567dc98cfa16e
hoodboy <littlepark0107@gmail.com>
2021-07-13 10:02:18 +0900

    fix: vercel error

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import {PageWrapper} from '../../components/PageStyle';
