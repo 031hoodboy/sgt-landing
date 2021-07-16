d6713ec8803012e58d467c735135a09b7af184b3
hoodboy <littlepark0107@gmail.com>
2021-07-16 18:23:22 +0900

    feat: add patent page

--- src/Pages/Technology/Patent.js
+++ src/Pages/Technology/Patent.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 
 import {PageWrapper} from '../../components/PageStyle';
 import BackgroundImg from '../../assets/technologybackground.png';
-import {PageTitleWrpper, PageTitle, SubTitle2, SubTitle3 } from '../../components/PageStyle';
+import {PageTitleWrpper, PageTitle, SubTitle2 } from '../../components/PageStyle';
 import styled from '@emotion/styled';
 
 const Patent = () => {
@@ -16,41 +16,53 @@ const Patent = () => {
                 설비현황
                 <NavBarWrapper>
                     <Link to="/introduce" style={{textDecoration: 'none', color: "#000"}}>
-                        <SelectNavBlock>연구소 소개</SelectNavBlock>
+                        <NavBlock>연구소 소개</NavBlock>
                     </Link>
                     <Link to="/patent" style={{textDecoration: 'none', color: "#000"}}>
-                        <NavBlock>특허 및 인증</NavBlock>
+                        <SelectNavBlock>특허 및 인증</SelectNavBlock>
                     </Link>
                 </NavBarWrapper>
             </MainImg>
             <PageTitleWrpper>
-                <PageTitle>연구소 소개</PageTitle>
+                <PageTitle>특허 및 인증</PageTitle>
                 <SubTitle2>
                 고효율 태양광 셀과 고출력 태양광 모듈 신기술을 상용화 함으로써<br/>
                 화석 연료의 사용을 감축할 수 있도록 기술개발과 상용화에 매진하고 있습니다.
                 </SubTitle2>
-                <SubTitle3>
-                연구분야 : 수분 동결 건조 기술, 마이크로파 건조 기술, 소재 내후성 테스트 기술, 반도체 검사 장비 관리, 전밀 부품 가공 기술, 필름소재 관련 기술
-                </SubTitle3>
             </PageTitleWrpper>
             <BarkWrapper>
-                <Bar>가공 설비 : 3대</Bar>
                 <Blockwrapper>
                     <RemarkWrapper>
-                        <Block></Block>
                         <Remark>동결 건조 DEMO 장비 개발</Remark>
+                        <Block></Block>
                     </RemarkWrapper>
                     <RemarkWrapper>
+                        <Remark>동결 건조 DEMO 장비 개발</Remark>
                         <Block></Block>
-                        <Remark>제조사 : 남선기계</Remark>
-                        <Remark>규격 : 400-1060</Remark>
-                        <Remark>보유 현황 : 1대</Remark>
                     </RemarkWrapper>
                     <RemarkWrapper>
+                        <Remark>동결 건조 DEMO 장비 개발</Remark>
                         <Block></Block>
-                        <Remark>제조사 : 금광톱기계</Remark>
-                        <Remark>규격 : KHA-340</Remark>
-                        <Remark>보유 현황 : 1대</Remark>
+                    </RemarkWrapper>
+                    <RemarkWrapper>
+                        <Remark>동결 건조 DEMO 장비 개발</Remark>
+                        <Block></Block>
+                    </RemarkWrapper>
+                    <RemarkWrapper>
+                        <Remark>동결 건조 DEMO 장비 개발</Remark>
+                        <Block></Block>
+                    </RemarkWrapper>
+                    <RemarkWrapper>
+                        <Remark>동결 건조 DEMO 장비 개발</Remark>
+                        <Block></Block>
+                    </RemarkWrapper>
+                    <RemarkWrapper>
+                        <Remark>동결 건조 DEMO 장비 개발</Remark>
+                        <Block></Block>
+                    </RemarkWrapper>
+                    <RemarkWrapper>
+                        <Remark>동결 건조 DEMO 장비 개발</Remark>
+                        <Block></Block>
                     </RemarkWrapper>
                 </Blockwrapper>
             </BarkWrapper>
@@ -147,12 +159,11 @@ const BarkWrapper = styled.div`
   }
 `;
 
-
 const Blockwrapper = styled.div`
     width: 1088px;
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-between;
+
     margin: 5vh auto 10vh auto;
     @media screen and (max-width: 1220px) {
         width: 90vw;
@@ -161,9 +172,8 @@ const Blockwrapper = styled.div`
 `;
 
 const Block = styled.div`
-    border-bottom: 1px solid #D0D0D0;
     width: 353px;
-    height: 350px;
+    height: 425px;
     margin-bottom: 15px;
     @media screen and (max-width: 1220px) {
     width: 90vw;
@@ -174,13 +184,21 @@ const Block = styled.div`
 
 const Remark = styled.div`
     font-size: 18px;
-    padding: 5px 20px;
+    padding: 20px;
+    border-bottom: 1px solid #D0D0D0;
+    text-align: center;
+
 `;
 
 const RemarkWrapper = styled.div`
     border: 1px solid #DBDBDB;
     padding-bottom: 15px;
     margin-bottom: 5vh;
+    margin-right: 7.5px;
+    @media screen and (max-width: 1220px) {
+        margin: 0;
+        margin-bottom: 5vh;
+  }
 `;
 
 export default Patent;
