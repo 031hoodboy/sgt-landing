e6779191f2ff20e6d74ce618270c9df83861dba3
hoodboy <littlepark0107@gmail.com>
2021-07-16 17:58:55 +0900

    feat: add introduce page

--- src/Pages/Technology/Introduce.js
+++ src/Pages/Technology/Introduce.js
@@ -8,7 +8,7 @@ import BackgroundImg from '../../assets/technologybackground.png';
 import {PageTitleWrpper, PageTitle, SubTitle2, SubTitle3 } from '../../components/PageStyle';
 import styled from '@emotion/styled';
 
-const Processing = () => {
+const Introduce = () => {
     return (
         <PageWrapper>
             <Header/>
@@ -34,26 +34,81 @@ const Processing = () => {
                 </SubTitle3>
             </PageTitleWrpper>
             <BarkWrapper>
-                <Bar>가공 설비 : 3대</Bar>
+                <Bar>동결 건조 기술</Bar>
                 <Blockwrapper>
                     <RemarkWrapper>
                         <Block></Block>
-                        <Remark>제조사 : 화천기계</Remark>
-                        <Remark>규격 : HMT-1100</Remark>
-                        <Remark>보유 현황 : 2대</Remark>
+                        <Remark>동결 건조 DEMO 장비 개발</Remark>
                     </RemarkWrapper>
+                    <BlockContent>
+                        <Contnet>
+                            • 개발목적<br/>
+                            - 건조 시료의 수분 함유율 최소화 장비 사양<br/>
+                        </Contnet>
+                        <br/><br/>
+                        <Contnet>
+                            • 장비 특징<br/>
+                            - 시료 수분 함유율 1% 건조 [기존장비 3~5%]<br/>
+                            - 규격 및 용량  : 1,400[W] x 1,400[D] x 1,700[H], 20L급 [6단 선반]<br/>
+                        </Contnet>
+                        <br/><br/>
+                        <Contnet>
+                            • 장비 사용 용도<br/>
+                            - 식품 및 제약 등 바이오 산업 분야 시료 건조<br/>
+                            - 반도체/디스플레이 고분자 소재 건조<br/>
+                        </Contnet>
+                    </BlockContent>
+                </Blockwrapper>
+                <Blockwrapper>
                     <RemarkWrapper>
                         <Block></Block>
-                        <Remark>제조사 : 남선기계</Remark>
-                        <Remark>규격 : 400-1060</Remark>
-                        <Remark>보유 현황 : 1대</Remark>
+                        <Remark>실시간 수분 측정 장치 개발</Remark>
                     </RemarkWrapper>
+                    <BlockContent>
+                        <Contnet>
+                            • 개발목적<br/>
+                            - 건조장치 내부 수분 함유량 실시간 측정 및 제어<br/>
+                        </Contnet>
+                        <br/><br/>
+                        <Contnet>
+                            • 장비 특징<br/>
+                            - Camber 내부 온도 -50℃~+60℃ 제어<br/>
+                            - 규격 및 용량 : 800[W] x 800[D] x 1,600[H], 4L급 [2단 선반]<br/>
+                        </Contnet>
+                        <br/><br/>
+                        <Contnet>
+                            • 장비 사용 용도<br/>
+                            - 건조 시료별 수분 함유량 분석 및 최적 레시피 제작<br/>
+                            - 식품, 제약 등 바이오산업 및 소재 산업 수분 제어 기술 활용<br/>
+                        </Contnet>
+                    </BlockContent>
+                </Blockwrapper>
+                <Bar>금속 플레이트 및 부품 측면 홀/탭 가공</Bar>
+                <Blockwrapper>
                     <RemarkWrapper>
                         <Block></Block>
-                        <Remark>제조사 : 금광톱기계</Remark>
-                        <Remark>규격 : KHA-340</Remark>
-                        <Remark>보유 현황 : 1대</Remark>
+                        <Remark>동결 건조 DEMO 장비 개발</Remark>
                     </RemarkWrapper>
+                    <BlockContent>
+                        <Contnet>
+                            • 개발목적<br/>
+                            - 금속소재부품 측면 가공 효율성 및 생산성 향상<br/>
+                        </Contnet>
+                        <br/><br/>
+                        <Contnet>
+                            • 장비 특징<br/>
+                            - 규격 : 2,400[W] x 1,650[D] x 1,800[H]<br/>
+                            - 작업범위 : Max. 1,500[W] x 800[D] x 20[T]<br/>
+                            - DRILLING 최대 ~ 13, TAPPING 최대 ~ M12<br/>
+                            - CHUK : BT30, SPINDLE : 9,000 Rpm<br/>
+                        </Contnet>
+                        <br/><br/>
+                        <Contnet>
+                            • 장비 사용 용도<br/>
+                            - 반도체/디스플레이 장비 부품 측면 홀 및 탭 정밀 가공<br/>
+                            - 산업자동화 및 물류 장비 대형 플레이트 측면 가공<br/>
+                        </Contnet>
+                    </BlockContent>
                 </Blockwrapper>
             </BarkWrapper>
             <Footer/>
@@ -184,4 +239,23 @@ const RemarkWrapper = styled.div`
     padding-bottom: 15px;
     margin-bottom: 5vh;
 `;
-export default Processing;
+
+const BlockContent = styled.div`
+    width: 594px;
+    margin-bottom: 15px;
+    @media screen and (max-width: 1220px) {
+    width: 90vw;
+    max-width: 360px;
+    height: 316px;
+  }
+`;
+
+const Contnet = styled.div`
+    font-size: 20px;
+    line-height: 32px;
+    @media screen and (max-width: 780px) {
+        font-size: 16px;
+  }
+`;
+
+export default Introduce;
