92fb131ac635d344f1f5b503e9bbbad064c2c2bf
hoodboy <littlepark0107@gmail.com>
2021-07-30 01:21:50 +0900

    feat: quote contact form del

--- src/Pages/CustomerSupport/QuoteContact.js
+++ src/Pages/CustomerSupport/QuoteContact.js
@@ -3,12 +3,12 @@ import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import {Link} from 'react-router-dom';
-import Email from '../../components/Email';
+// import Email from '../../components/Email';
 import {PageWrapper} from '../../components/PageStyle';
 import BackgroundImg from '../../assets/customersupportbackground.png';
 import {PageTitleWrpper, PageTitle, SubTitle2 } from '../../components/PageStyle';
 import styled from '@emotion/styled';
-
+import MailImg from '../../assets/mail.png'
 const QuoteContact = () => {
     return (
         <PageWrapper>
@@ -19,20 +19,22 @@ const QuoteContact = () => {
                     <Link to="/quote-contact" style={{textDecoration: 'none', color: "#000"}}>
                         <SelectNavBlock>견적문의</SelectNavBlock>
                     </Link>
-                    <Link to="/recruitment" style={{textDecoration: 'none', color: "#000"}}>
+                    {/* <Link to="/recruitment" style={{textDecoration: 'none', color: "#000"}}>
                         <NavBlock>채용안내</NavBlock>
-                    </Link>
+                    </Link> */}
                 </NavBarWrapper>
             </MainImg>
             <PageTitleWrpper>
                 <PageTitle>견적문의</PageTitle>
                 <SubTitle2>
-                고효율 태양광 셀과 고출력 태양광 모듈 신기술을 상용화 함으로써<br/>
-                화석 연료의 사용을 감축할 수 있도록 기술개발과 상용화에 매진하고 있습니다.
+                    가공 및 제품 견적 등 문의사항의 경우 아래의 메일로 연락주시길 바랍니다.
                 </SubTitle2>
-                <Email/>
+                {/* <Email/> */}
             </PageTitleWrpper>
-
+            <Blockwrapper>
+                <Mail/>
+                123123@sgtmail.com
+            </Blockwrapper>
             <Footer/>
         </PageWrapper>
     );
@@ -69,8 +71,6 @@ const NavBarWrapper = styled.div`
     font-size: 16px;
     font-weight: 400;
     top: calc(30vh + 56px);
-    box-shadow:  0px 3px 6px #d9d9d9;
-
     @media screen and (max-width: 1024px) {
         width: calc(90vw + 3px);
         top: calc(23vh); 
@@ -79,7 +79,7 @@ const NavBarWrapper = styled.div`
 `;
 
 const NavBlock = styled.div`
-    width: 30vw;
+    width: 60vw;
     height: 58px;
     border-left: 1px solid #D9D9D9;
     display: flex;
@@ -88,7 +88,7 @@ const NavBlock = styled.div`
     background: #fff;
     border-bottom: 1px solid #D9D9D9;
     @media screen and (max-width: 1024px) {
-    width: 45vw;
+    width: 90vw;
     height: 40px; 
     font-size: 14px;
     }
@@ -99,5 +99,40 @@ const SelectNavBlock = styled(NavBlock)`
     color: #fff;
     border-left: 1px solid #930E14;
 `;
+
+const Mail = styled.div`
+    width: 89px;
+    height: 59px;
+    margin: 0;
+    background-image: url(${MailImg});
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center; 
+    margin-top: 10px;
+    margin-right: 3vw;
+    @media screen and (max-width: 780px) {
+    width: 75px;
+    height: 50px;
+    margin: 0;
+    margin-bottom: 5vh;
+  }
+`;
+
+const Blockwrapper = styled.div`
+    width: 1088px;
+    display: flex;
+    flex-wrap: wrap;
+    margin: 15vh auto 20vh auto;
+    font-size: 40px;
+    color: #bfbfbf;
+    justify-content: center;
+    @media screen and (max-width: 1220px) {
+        width: 90vw;
+        flex-direction: column;
+        font-size: 28px;
+        align-items: center;
+  }
+`;
+
 
 export default QuoteContact;
