e1f42b7c3d4691988a98a58069cffe94367e4e6d
hoodboy <littlepark0107@gmail.com>
2021-07-30 21:47:08 +0900

    feat: add pagination

--- src/Pages/About/OrganizeChart.js
+++ src/Pages/About/OrganizeChart.js
@@ -1,96 +1,194 @@
-import React from 'react';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
-import {Link} from 'react-router-dom';
-
-import {PageWrapper} from '../../components/PageStyle';
-import BackgroundImg from '../../assets/aboutbackground.png';
-import {PageTitleWrpper, PageTitle, NavBarWrapper, NavBlock, SelectNavBlock} from '../../components/PageStyle';
+import React, {Component} from 'react'
+import axios from 'axios'
+import ReactPaginate from 'react-paginate';
 import styled from '@emotion/styled';
-// import ProfileImg from '../../assets/profile.png';
-import Chart from '../../assets/organizechart.png'
+import NewsImg1 from '../../assets/newscard1.png';
 
-const OrganizeChart = () => {
-    return (
-        <PageWrapper>
-            <Header/>
-            <MainImg>
-                회사소개
-                <NavBarWrapper>
-                    <Link to="/greet" style={{textDecoration: 'none', color: "#000"}}>
-                        <SelectNavBlock>인사말</SelectNavBlock>
-                    </Link>
-                    <Link to="/vision" style={{textDecoration: 'none', color: "#000"}}>
-                        <NavBlock>경영이념</NavBlock>
-                    </Link>
-                    <Link to="/history" style={{textDecoration: 'none', color: "#000"}}>
-                        <NavBlock>연혁</NavBlock>
-                    </Link>
-                    {/* <Link to="/business-partner" style={{textDecoration: 'none', color: "#000"}}>
-                        <NavBlock>사업파트너</NavBlock>
-                    </Link> */}
-                    <Link to="/way-to-come" style={{textDecoration: 'none', color: "#000"}}>
-                        <NavBlock>오시는 길</NavBlock>
-                    </Link>
-                        {/* <Link to="/organize-chart" style={{textDecoration: 'none', color: "#000"}}>
-                    <NavBlock>조직도</NavBlock>
-                    </Link> */}
-                </NavBarWrapper>
-            </MainImg>
-            <PageTitleWrpper>
-                <PageTitle>조직도</PageTitle>
-            </PageTitleWrpper>
-            <ChartImg></ChartImg>
-            <Footer/>
-        </PageWrapper>
-    );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            offset: 0,
+            data: [],
+            perPage: 3,
+            currentPage: 0
+        };
+        this.handlePageClick = this
+            .handlePageClick
+            .bind(this);
+    }
+    receivedData() {
+        axios
+            .get(`https://www.sgtapi.tk/news/newslist.do`)
+            .then(res => {
+
+                const data = res.data;
+                const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
+                const postData = slice.map(newscards => <NewsCardBlock key={newscards.idx} 
+                    // onClick={() =>
+                    //     history.push(
+                    //     {
+                    //         pathname: `/news-info/${newscards.idx}`,
+                    //         props: {id: newscards.idx}
+                    //     })}    
+                >
+                    <NewsImg></NewsImg>
+                    <Line/>
+                    <NewsContentWrapper>
+                    <NewsContentTitle>{newscards.news_sub}</NewsContentTitle>
+                    {newscards.idx}
+                    <NewsContent>{newscards.newscon}</NewsContent>
+                    </NewsContentWrapper>
+                </NewsCardBlock>
+                )
+
+                this.setState({
+                    pageCount: Math.ceil(data.length / this.state.perPage),
+                   
+                    postData
+                })
+            });
+    }
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        }, () => {
+            this.receivedData()
+        });
+
+    };
+
+    componentDidMount() {
+        this.receivedData()
+    }
+    render() {
+        return (
+            <div>
+                {this.state.postData}
+                <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
+            </div>
+
+        )
+    }
 }
 
-const MainImg = styled.div`
-    width: 100%;
-    height: 30vh;
-    background: #282D27;
-    background-image: url(${BackgroundImg});
+
+
+const NavBarWrapper = styled.div`
+    width: 60vw;
+    height: 58px;
+    display: flex;
+    margin: 0 auto;
+    position: absolute;
+    color: #000;
+    font-size: 16px;
+    font-weight: 400;
+    top: calc(30vh + 56px);
+    @media screen and (max-width: 1024px) {
+        width: calc(90vw + 3px);
+        top: calc(23vh); 
+        height: 40px;
+    }
+`;
+
+const NavBlock = styled.div`
+    width: 30vw;
+    height: 58px;
+    border-left: 1px solid #D9D9D9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #fff;
+    border-bottom: 1px solid #D9D9D9;
+    @media screen and (max-width: 1024px) {
+    width: 45vw;
+    height: 40px; 
+    font-size: 14px;
+    }
+`;
+
+const SelectNavBlock = styled(NavBlock)`
+    background: #930E14;
+    color: #fff;
+    border-left: 1px solid #930E14;
+`;
+
+const NewsCardWrapper = styled.div`
+    display: flex;
+    width: 90vw;
+    max-width: 1088px;
+    margin: 0 auto;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 10vh 0;
+    @media screen and (max-width: 880px) {
+        justify-content: space-around;
+        padding: 0;
+        margin-bottom: 10vh;
+  }
+`;
+ 
+const NewsCardBlock = styled.div`
+    max-width: 350px;
+    border: 1px solid #D8D8D8;
+    margin-top: 5vh;
+`;
+
+const NewsImg = styled.div`
+    min-height: 198px;
+    max-width: 350px;
+    background: #fff;
+    background-image: url(${NewsImg1});
     background-size: cover;
     background-repeat: no-repeat;
     background-position: center; 
     display: flex;
     align-items: center;
-    justify-content: center;
     flex-direction: column;
-    color: #fff;
-    font-size: 45px;
-    font-weight: 600;
-    @media screen and (max-width: 1024px) {
-        height: 17vh;
-        font-size: 26px;
-  }
-`;
-
-const ChartImg = styled.div`
-    width: 60vw;
-    max-width: 1021.67px;
-    height: 70vw;
-    max-height: 689px;
-    margin: 5vh auto;
-    background-image: url(${Chart});
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-position: center; 
-    display: flex;
-    align-items: flex-end;
-    justify-content: center;
-    flex-direction: column;
-    margin-bottom: 20vh;
-    @media screen and (max-width: 1220px) {
+    @media screen and (max-width: 840px) {
         width: 90vw;
-        min-width: 317.5px;
-        height: 70vw;
-        min-height: 215px;
-        margin: 3vh auto;
-        margin-bottom: 10vh;
+  }
+  `;
 
+const Line = styled.div`
+    min-height: 5px;
+    width: 90vw;
+    max-width: 350px;
+    background: #98272B;
+    @media screen and (max-width: 840px) {
+        width: 90vw;
   }
 `;
 
-export default OrganizeChart;
+const NewsContentWrapper = styled.div`
+    width: 80vw;
+    max-width: 305px;
+    margin: 0 auto;
+    padding: 30px 0;
+`;
+
+const NewsContentTitle = styled.div`
+    font-size: 18px;
+`;
+
+const NewsContent = styled.div`
+    margin-top: 25px;
+    display: flex;
+    flex-wrap: wrap;
+    font-size: 14px;
+    max-width: 305px;
+`;
--- src/Pages/PRCenter/News.js
+++ src/Pages/PRCenter/News.js
@@ -1,4 +1,8 @@
-import React, {useState, useEffect} from 'react';
+/* global history */
+/* global location */
+/* global window */
+/* eslint no-restricted-globals: ["off"] */
+import React, {useState, useEffect, Component} from 'react';
 import { useHistory } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
@@ -11,44 +15,69 @@ import {PageTitleWrpper, PageTitle, SubTitle2} from '../../components/PageStyle'
 import styled from '@emotion/styled';
 import NewsImg1 from '../../assets/newscard1.png';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            offset: 0,
+            data: [],
+            perPage: 6,
+            currentPage: 0
+        };
+        this.handlePageClick = this
+            .handlePageClick
+            .bind(this);
+    }
+    receivedData() {
+        axios
+            .get(`https://www.sgtapi.tk/news/newslist.do`)
+            .then(res => {
+                const data = res.data;
+                const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
+                const postData = slice.map(newscards => 
+                <NewsCardBlock key={newscards.idx} 
+                    // onClick={() =>
+                    //     history.push(
+                    //     {
+                    //         pathname: `/news-info/${newscards.idx}`,
+                    //         props: {id: newscards.idx}
+                    //     })}    
+                >
+                    <NewsImg></NewsImg>
+                    <Line/>
+                    <NewsContentWrapper>
+                    <NewsContentTitle>{newscards.news_sub}</NewsContentTitle>
+                    {newscards.idx}
+                    <NewsContent>{newscards.newscon}</NewsContent>
+                    </NewsContentWrapper>
+                </NewsCardBlock>
+                )
 
-const News = () => {
+                this.setState({
+                    pageCount: Math.ceil(data.length / this.state.perPage),
+                   
+                    postData
+                })
+            });
+    }
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        }, () => {
+            this.receivedData()
+        });
 
-
-    const [newscard, setNewscard] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-
-    const history = useHistory();
-
-    const [page, setPage] = useState(1);
-    const handlePageChange = (page) => { setPage(page); };
-  
-    useEffect(() => {
-    const fetchNews = async () => {
-        try {
-        // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-        setError(null);
-        setNewscard(null);
-        // loading 상태를 true 로 바꿉니다.
-        setLoading(true);
-        const response = await axios.get(
-            'https://www.sgtapi.tk/news/newslist.do'
-        );
-        setNewscard(response.data); // 데이터는 response.data 안에 들어있습니다.
-        } catch (e) {
-        setError(e);
-        }
-        setLoading(false);
     };
 
-    fetchNews();
-    }, []);
-
-    if (loading) return <div>로딩중..</div>;
-    if (error) return <div>에러가 발생했습니다</div>;
-    if (!newscard) return null;
+    componentDidMount() {
+        this.receivedData()
+    }
+    render() {
 
     return (
         <PageWrapper>
@@ -72,30 +101,27 @@ const News = () => {
                 </SubTitle2>
             </PageTitleWrpper>
             <NewsCardWrapper>
-            {newscard.map(newscards => (
-                    <NewsCardBlock key={newscards.idx} 
-                        onClick={() =>
-                            history.push(
-                            {
-                                pathname: `/news-info/${newscards.idx}`,
-                                props: {id: newscards.idx}
-                            }
-                            )
-                        }    
-                    >
-                        <NewsImg></NewsImg>
-                        <Line/>
-                        <NewsContentWrapper>
-                        <NewsContentTitle>{newscards.news_sub}</NewsContentTitle>
-                        <NewsContent>{newscards.newscon}</NewsContent>
-                        </NewsContentWrapper>
-                    </NewsCardBlock>
-                ))}
-                <Pagination activePage={page} itemsCountPerPage={4} totalItemsCount={100} pageRangeDisplayed={5} prevPageText={"‹"} nextPageText={"›"} onChange={handlePageChange} />
-                </NewsCardWrapper>
+                {this.state.postData}
+            </NewsCardWrapper>
+                            <Paginate>
+                 <ReactPaginate
+                    previousLabel={"<"}
+                    nextLabel={">"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}
+                />
+                </Paginate>
             <Footer/>
         </PageWrapper>
     );
+}
 }
 
 const MainImg = styled.div`
@@ -163,7 +189,7 @@ const NewsCardWrapper = styled.div`
     width: 90vw;
     max-width: 1088px;
     margin: 0 auto;
-    justify-content: space-between;
+    justify-content: space-around;
     flex-wrap: wrap;
     padding: 10vh 0;
     @media screen and (max-width: 880px) {
@@ -224,4 +250,6 @@ const NewsContent = styled.div`
     max-width: 305px;
 `;
 
-export default News;
+const Paginate = styled.div`
+    margin: 5vh auto;
+`;
